b57eeb1d30a6f740b49a9096eff6a520756d3bdf
Serge Klochkov <3175289+slvrtrn@users.noreply.github.com>
2023-02-07 12:12:44 +0100

    Address Metabase code review comments (#135)
    * Tests refactoring, fix minor DateTime64 match issue
    
    * Add ClickHouse TLS server and basic TLS test
    
    * Add clickhouse_tls to CI

--- test/metabase/test/data/clickhouse.clj
+++ test/metabase/test/data/clickhouse.clj
@@ -13,6 +13,17 @@
 
 (sql-jdbc.tx/add-test-extensions! :clickhouse)
 
+(def product-name "metabase/1.0.1 clickhouse-jdbc/0.3.2-patch-11")
+(def default-connection-params {:classname "com.clickhouse.jdbc.ClickHouseDriver"
+                                :subprotocol "clickhouse"
+                                :subname "//localhost:8123/default"
+                                :user "default"
+                                :password ""
+                                :ssl false
+                                :use_no_proxy false
+                                :use_server_time_zone_for_dates true
+                                :client_name product-name})
+
 (defmethod sql.tx/field-base-type->sql-type [:clickhouse :type/Boolean]    [_ _] "Boolean")
 (defmethod sql.tx/field-base-type->sql-type [:clickhouse :type/BigInteger] [_ _] "Int64")
 (defmethod sql.tx/field-base-type->sql-type [:clickhouse :type/Char]       [_ _] "String")
