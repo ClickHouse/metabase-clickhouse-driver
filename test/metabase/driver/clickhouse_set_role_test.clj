6fb834aa3cebd4c31d707590cb9cafc0a3a901af
slvrtrn <hypnoash@gmail.com>
2024-04-18 12:43:55 +0200

    WIP set role on cluster

--- test/metabase/driver/clickhouse_set_role_test.clj
+++ test/metabase/driver/clickhouse_set_role_test.clj
@@ -7,8 +7,9 @@
             [metabase.driver.sql-jdbc.connection :as sql-jdbc.conn]
             [metabase.driver.sql-jdbc.execute :as sql-jdbc.execute]
             [metabase.test :as mt]
-            [metabase.test.data [interface :as tx]]
             [metabase.test.data.clickhouse :as ctd]))
+
+(set! *warn-on-reflection* true)
 
 (defn- set-role-test!
   [details-map]
@@ -26,7 +27,16 @@
       (sql-jdbc.execute/do-with-connection-with-options
        :clickhouse spec nil
        (fn [^java.sql.Connection conn]
-         (driver/set-role! :clickhouse conn default-role)))
+         (driver/set-role! :clickhouse conn default-role)
+         (fn [^java.sql.Connection conn]
+           (driver/set-role! :clickhouse conn default-role)
+           (with-open [stmt (.prepareStatement conn "SELECT * FROM `metabase_test_role_db`.`some_table` ORDER BY i ASC;")
+                       rset (.executeQuery stmt)]
+             (is (.next rset) true)
+             (is (.getInt rset 1) 42)
+             (is (.next rset) true)
+             (is (.getInt rset 1) 144)
+             (is (.next rset) false)))))
       (is true))
     (testing "throws when assigning a non-existent role"
       (is (thrown? Exception
@@ -35,34 +45,30 @@
                     (fn [^java.sql.Connection conn]
                       (driver/set-role! :clickhouse conn "asdf"))))))))
 
-(deftest clickhouse-set-role-single-node
+(deftest clickhouse-set-role
   (mt/test-driver
    :clickhouse
-   (let [statements ["DROP DATABASE metabase_test_role_db"
-                     "CREATE DATABASE IF NOT EXISTS `metabase_test_role_db`;"
-                     "CREATE OR REPLACE TABLE `metabase_test_role_db`.`some_table` (i Int32) ENGINE = MergeTree ORDER BY (i);"
-                     "INSERT INTO `metabase_test_role_db`.`some_table` VALUES (42), (144);"
-                     "CREATE ROLE IF NOT EXISTS `metabase_test_role`;"
-                     "CREATE USER IF NOT EXISTS `metabase_test_user` NOT IDENTIFIED;"
-                     "GRANT SELECT ON `metabase_test_role_db`.* TO `metabase_test_role`;"
-                     "GRANT `metabase_test_role` TO `metabase_test_user`;"]]
-     (ctd/exec-statements statements {})
-     (set-role-test! {:user "metabase_test_user"}))))
-
-;;(merge {:user "metabase_test_user"}
-                            ;;(tx/dbdef->connection-details :clickhouse :db {:database-name "default"}))
-(deftest clickhouse-set-role-on-premise-cluster
-  (mt/test-driver
-   :clickhouse
-   (let [statements ["DROP DATABASE IF EXISTS metabase_test_role_db ON CLUSTER 'test_cluster'"
-                     "CREATE DATABASE `metabase_test_role_db` ON CLUSTER 'test_cluster';"
-                     "CREATE TABLE `metabase_test_role_db`.`some_table` ON CLUSTER 'test_cluster' (i Int32)
-                      ENGINE ReplicatedMergeTree('/clickhouse/{cluster}/tables/{database}/{table}/{shard}', '{replica}')
-                      ORDER BY (i);"
-                     "INSERT INTO `metabase_test_role_db`.`some_table` VALUES (42), (144);"
-                     "CREATE ROLE IF NOT EXISTS `metabase_test_role` ON CLUSTER 'test_cluster';"
-                     "CREATE USER IF NOT EXISTS `metabase_test_user` ON CLUSTER 'test_cluster' NOT IDENTIFIED;"
-                     "GRANT ON CLUSTER 'test_cluster' SELECT ON `metabase_test_role_db`.* TO `metabase_test_role`;"
-                     "GRANT ON CLUSTER 'test_cluster' `metabase_test_role` TO `metabase_test_user`;"]]
-     (ctd/exec-statements statements {:port 8127})
-     (set-role-test! {:user "metabase_test_user" :port 8127}))))
+   (testing "single node"
+     (let [statements ["DROP DATABASE metabase_test_role_db"
+                       "CREATE DATABASE IF NOT EXISTS `metabase_test_role_db`;"
+                       "CREATE OR REPLACE TABLE `metabase_test_role_db`.`some_table` (i Int32) ENGINE = MergeTree ORDER BY (i);"
+                       "INSERT INTO `metabase_test_role_db`.`some_table` VALUES (42), (144);"
+                       "CREATE ROLE IF NOT EXISTS `metabase_test_role`;"
+                       "CREATE USER IF NOT EXISTS `metabase_test_user` NOT IDENTIFIED;"
+                       "GRANT SELECT ON `metabase_test_role_db`.* TO `metabase_test_role`;"
+                       "GRANT `metabase_test_role` TO `metabase_test_user`;"]]
+       (ctd/exec-statements statements {})
+       (set-role-test! {:user "metabase_test_user"})))
+   (testing "on-premise cluster"
+     (let [statements ["DROP DATABASE IF EXISTS metabase_test_role_db ON CLUSTER 'test_cluster'"
+                       "CREATE DATABASE `metabase_test_role_db` ON CLUSTER 'test_cluster';"
+                       "CREATE TABLE `metabase_test_role_db`.`some_table` ON CLUSTER 'test_cluster' (i Int32)
+                           ENGINE ReplicatedMergeTree('/clickhouse/{cluster}/tables/{database}/{table}/{shard}', '{replica}')
+                           ORDER BY (i);"
+                       "INSERT INTO `metabase_test_role_db`.`some_table` VALUES (42), (144);"
+                       "CREATE ROLE IF NOT EXISTS `metabase_test_role` ON CLUSTER 'test_cluster';"
+                       "CREATE USER IF NOT EXISTS `metabase_test_user` ON CLUSTER 'test_cluster' NOT IDENTIFIED;"
+                       "GRANT ON CLUSTER 'test_cluster' SELECT ON `metabase_test_role_db`.* TO `metabase_test_role`;"
+                       "GRANT ON CLUSTER 'test_cluster' `metabase_test_role` TO `metabase_test_user`;"]]
+       (ctd/exec-statements statements {:port 8127})
+       (set-role-test! {:user "metabase_test_user" :port 8127})))))
